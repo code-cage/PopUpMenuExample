aa2eb47006fb8335e7cd70e4d4499bcf7935ce79
shivnaytiwari <shivnaytiwari@gmail.com>
2018-01-13 00:35:17 +0530

    last update

--- app/src/main/java/com/example/sandeep/popupmenuexample/MainActivity.java
+++ app/src/main/java/com/example/sandeep/popupmenuexample/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.sandeep.popupmenuexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,31 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater=menu.getMenuInflater();
         inflater.inflate(R.menu.pop_up_menu,menu.getMenu());
         menu.show();
+
+        menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id=menuItem.getItemId();
+
+                switch (id){
+                    case R.id.item1:
+                        Toast.makeText(MainActivity.this,"item 1 clicked",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.item2 :
+                        Toast.makeText(MainActivity.this,"item 2 clicked",Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.item3 :
+                        Toast.makeText(MainActivity.this,"item 3 clicked",Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+
+
+                return false;
+            }
+        });
 
     }
 }
